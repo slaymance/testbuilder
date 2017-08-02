2562c1df623b167fa0223f3bf11ebd286195c389
slaymance <shane.laymance@gmail.com>
2017-08-01 23:26:36 -0700

    Format conditionals for better readability

--- detectNetwork.js
+++ detectNetwork.js
@@ -27,7 +27,8 @@ var detectNetwork = function(cardNumber) {
 
   // Diner's Club
   var dinersLength = cardArray.length === 14;
-  var dinersPrefix = twoPrefix >= 38 && twoPrefix <= 39
+  var dinersPrefix = twoPrefix >= 38 && twoPrefix <= 39;
+
   if(dinersLength && dinersPrefix) {
     return 'Diner\'s Club';
   }
@@ -35,15 +36,21 @@ var detectNetwork = function(cardNumber) {
   // American Express
   var amexLength = cardArray.length === 15;
   var amexPrefix = twoPrefix === 34 || twoPrefix === 37;
+
   if(amexLength && amexPrefix) {
     return 'American Express';
   }
 
   // Visa
-  var visaLength = cardArray.length === 13 || cardArray.length === 16 
-    || cardArray.length === 19;
-  var visaPrefix = cardArray[0] === 4 && fourPrefix !== 4903 && fourPrefix !== 
-    4905 && fourPrefix !== 4911 && fourPrefix !== 4936;
+  var visaLength = cardArray.length === 13 || 
+    cardArray.length === 16 || 
+    cardArray.length === 19;
+  var visaPrefix = cardArray[0] === 4 && 
+    fourPrefix !== 4903 && 
+    fourPrefix !== 4905 && 
+    fourPrefix !== 4911 && 
+    fourPrefix !== 4936;
+
   if(visaLength && visaPrefix) {
     return 'Visa';
   }
@@ -51,40 +58,59 @@ var detectNetwork = function(cardNumber) {
   // MasterCard
   var masterLength = cardArray.length === 16;
   var masterPrefix = twoPrefix >= 51 && twoPrefix <= 55;
+
   if(masterLength && masterPrefix) {
     return 'MasterCard'
   }
 
   // Discover
   var discoverLength = cardArray.length === 16 || cardArray.length === 19;
-  var discoverPrefix = twoPrefix === 65 || threePrefix >= 644 && threePrefix <=
-    649 || fourPrefix === 6011;
+  var discoverPrefix = twoPrefix === 65 || 
+    threePrefix >= 644 && 
+    threePrefix <= 649 || 
+    fourPrefix === 6011;
+
   if(discoverLength && discoverPrefix) {
     return 'Discover';
   }
 
   // Maestro
   var maestroLength = cardArray.length >= 12 && cardArray.length <= 19;
-  var maestroPrefix = fourPrefix === 5018 || fourPrefix === 5020 
-    || fourPrefix === 5038 || fourPrefix === 6304
+  var maestroPrefix = fourPrefix === 5018 || 
+    fourPrefix === 5020 || 
+    fourPrefix === 5038 || 
+    fourPrefix === 6304;
+
   if(maestroLength && maestroPrefix) {
     return 'Maestro';
   }
 
   // China UnionPay
   var chinaLength = cardArray.length >= 16 && cardArray.length <= 19;
-  var chinaPrefix = threePrefix >= 624 && threePrefix <= 626 || fourPrefix >= 
-    6282 && fourPrefix <= 6288 || sixPrefix >= 622126 && sixPrefix <= 622925;
+  var chinaPrefix = threePrefix >= 624 && 
+    threePrefix <= 626 || 
+    fourPrefix >= 6282 && 
+    fourPrefix <= 6288 || 
+    sixPrefix >= 622126 && 
+    sixPrefix <= 622925;
+
   if(chinaLength && chinaPrefix) {
     return 'China UnionPay';
   }
 
   // Switch
-  var switchLength = cardArray.length === 16 || cardArray.length === 18 
-    || cardArray.length === 19;
-  var switchPrefix = fourPrefix === 4903 || fourPrefix === 4905 || fourPrefix 
-    === 4911 || fourPrefix === 4936 || fourPrefix === 6333 || fourPrefix === 6759
-    || sixPrefix === 564182 || sixPrefix === 633110;
+  var switchLength = cardArray.length === 16 || 
+    cardArray.length === 18 || 
+    cardArray.length === 19;
+  var switchPrefix = fourPrefix === 4903 || 
+    fourPrefix === 4905 || 
+    fourPrefix === 4911 || 
+    fourPrefix === 4936 || 
+    fourPrefix === 6333 || 
+    fourPrefix === 6759 || 
+    sixPrefix === 564182 || 
+    sixPrefix === 633110;
+    
   if(switchLength && switchPrefix) {
     return 'Switch';
   }
